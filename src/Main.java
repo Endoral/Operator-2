dc81fd926fe20b06dcabfd80ac0e478f132a6d03
phant <roma0206@yandex.ru>
2023-01-14 16:25:44 +0300

    задача 9

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,36 @@ public class Main {
     public static void main(String[] args) {
         task1();
     }
-        public static void task1() {
+
+    public static void task1() {
         System.out.println("Условный оператор задача 8");
         int OS = 1;
-                if (OS == 0){
-            System.out.println ("Установите версию приложения для iOS по ссылке");
-        }else{
-            System.out.println( "Установите версию приложения для Android по ссылке");
+        if (OS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
         task2();
-            }
-            public static void task2() {
-                System.out.println("Условный оператор задача 9");
-
     }
+
+    public static void task2() {
+        System.out.println("Условный оператор задача 9");
+        int clientDeviceYear = 2015;
+        int IOS1 = 0;
+        int android1 = 0;
+        int clientOS = 0;
+if (clientOS == IOS1 && (clientDeviceYear<=2015 || clientDeviceYear <=2015)) {
+System.out.println("Установите версию приложения IOS по ссылке");
+}else{
+System.out.println( "Установите облегченную версию");
 }
+if (clientOS == android1 && (clientDeviceYear<=2015 || clientDeviceYear <=2015)) {
+System.out.println("Установите версию приложения андроид по ссылке");
+}else{
+System.out.println( "Установите облегченную версию");
+        }
+
+
+
+            }
+        }
