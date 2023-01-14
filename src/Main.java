53dafcfb1b59c89b623c913a684a40cdce4bcc8a
phant <roma0206@yandex.ru>
2023-01-14 15:29:52 +0300

    задача 9

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+        public static void task1() {
         System.out.println("Условный оператор задача 8");
+        int OS = 1;
+                if (OS == 0){
+            System.out.println ("Установите версию приложения для iOS по ссылке");
+        }else{
+            System.out.println( "Установите версию приложения для Android по ссылке");
+        }
+        task2();
+            }
+            public static void task2() {
+                System.out.println("Условный оператор задача 9");
 
     }
 }
